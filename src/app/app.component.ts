395c74baa0c6d2469c660ea21f04eb16fcc90fab
Saket <skate056@gmail.com>
2017-09-11 13:05:32 +0100

    Add flags for isProduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from './environment'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  isProduction = environment.production
+  isOverridden = environment.overridden
 }
